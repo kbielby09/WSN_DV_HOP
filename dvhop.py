eb546a64c4a53650d1233e614fd182a27b0314ec
kbielby09 <kbielby09@gmail.com>
2021-12-17 12:32:02 -0500

    modified trilateration function

--- dvhop.py
+++ dvhop.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 maxiumumHopDistance = 20
 averageHopSize      = 15
 
+
+
 class Node:
     def __init__(self, x, y, name):
         self.isBeacon = False
@@ -16,8 +18,37 @@ class Node:
         self.localizedPosition = (0, 0)
         self.DistanceTable = {}
 
-    def trilaterate(self, point1, point2, point3, r1, r2, r3):
-        print("TODO")
+    # def trilaterate(self, point1, point2, point3, r1, r2, r3):
+        # print("TODO")
+
+    # get norm of a vector
+    def norm(self, p):
+        return pow(pow(p.x, 2) + pow(p.y, 2), 0.5)
+
+
+    # def trilaterate(self, point1, point2, point3, r1, r2, r3):
+    #     resultPose = point()
+    #     # Unit vector from point1 to point2
+    #     p2p1_distance = pow(pow(point2.x-point1.x,2)+pow(point2.y-point1.y,2),0.5)
+    #     ex = point((point2.x-point1.x)/p2p1_distance,(point2.y-point1.y)/p2p1_distance)
+    #     aux = point((point3.x-point1.x,point3.y-point1.y))
+    #     # Signed magnitude of x component
+    #     i = (ex.x * aux.x) + (ex.y * aux.y)
+    #     # Unit vector in y direction
+    #     aux2 = point(point3.x-point1.x - i * ex.x, point3.y-point1.y -i * ex.y)
+    #     ey = point(aux2.x / norm(aux2), aux2.y / norm(aux2))
+    #     # Signed magnitude of y component
+    #     j = ey.x * aux.x + ey.y * aux.y
+    #     # Coordinates
+    #     x = (r1 ** 2 - r2 ** 2 + p2p1_distance ** 2)/(2*p2p1_distance)
+    #     y = (r1 ** 2 - r3 ** 2 + i ** 2 + j ** 2)/((2*j)-(i*x/j))
+    #     # Resulting coordinates
+    #     finalX = point1.x + (x*ex.x) + (y*ey.x)
+    #     finalY = point1.y + (x*ex.y) + (y*ey.y)
+    #     resultPose.x = finalX
+    #     resultPose.y = finalY
+    #     return resultPose
+
 
     def getName(self):
         return self.nodeName
@@ -95,44 +126,85 @@ def NodeInRange(SendNode, ReceiveNode):
 
     return False
 
+class point(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+def norm(p):
+        return pow(pow(p.x, 2) + pow(p.y, 2), 0.5)
+
+
+def trilaterate(point1, point2, point3, r1, r2, r3):
+        resultPose = point(0, 0)
+        # Unit vector from point1 to point2
+        p2p1_distance = pow(pow(point2.x-point1.x,2)+pow(point2.y-point1.y,2),0.5)
+        ex = point((point2.x-point1.x)/p2p1_distance,(point2.y-point1.y)/p2p1_distance)
+        aux = point(point3.x-point1.x, point3.y-point1.y)
+        # Signed magnitude of x component
+        i = (ex.x * aux.x) + (ex.y * aux.y)
+        # Unit vector in y direction
+        aux2 = point(point3.x-point1.x - i * ex.x, point3.y-point1.y -i * ex.y)
+        ey = point(aux2.x / norm(aux2), aux2.y / norm(aux2))
+        # Signed magnitude of y component
+        j = ey.x * aux.x + ey.y * aux.y
+        # Coordinates
+        x = (r1 ** 2 - r2 ** 2 + p2p1_distance ** 2)/(2*p2p1_distance)
+        y = (r1 ** 2 - r3 ** 2 + i ** 2 + j ** 2)/((2*j)-(i*x/j))
+        # Resulting coordinates
+        finalX = point1.x + (x*ex.x) + (y*ey.x)
+        finalY = point1.y + (x*ex.y) + (y*ey.y)
+        resultPose.x = finalX
+        resultPose.y = finalY
+        return resultPose
     
 if __name__ == "__main__":
     NodeList = []
     numNodes = 100
 
-    # create 100 nodes with random positions in 100x100 grid
-    for i in range(0, 100): 
-        nodeName = "node " + str(i) 
-        xPos = random.randint(1, 100)
-        yPos = random.randint(1, 100)
+    r1 = 4
+    r2 = 3
+    r3 = 3.25
+    point1 = point(4,4)
+    point2 = point(9, 7)
+    point3 = point(9, 1)
+    finalPose = trilaterate(point1, point2, point3, r1,r2,r3)
+    print("Final x position: " + str(finalPose.x))
+    print("Final y position: " + str(finalPose.y))
 
-        NodeList.append(Node(xPos, yPos, nodeName))
+    # # create 100 nodes with random positions in 100x100 grid
+    # for i in range(0, 100): 
+    #     nodeName = "node " + str(i) 
+    #     xPos = random.randint(1, 100)
+    #     yPos = random.randint(1, 100)
 
-    # generate 10 random beacons for nodes
-    for i in range(0, 10):
-        randomIndex = random.randint(0, len(NodeList))  # get random index of node list
+    #     NodeList.append(Node(xPos, yPos, nodeName))
 
-        NodeList[randomIndex].setAsBeacon()
+    # # generate 10 random beacons for nodes
+    # for i in range(0, 10):
+    #     randomIndex = random.randint(0, len(NodeList))  # get random index of node list
 
-    # perform dvhop starting with beacon nodes
-    for SendNode in NodeList:
-        hopCount = 0
+    #     NodeList[randomIndex].setAsBeacon()
 
-        # check if node is beacon
-        if SendNode.getBeacon():
-            beaconName     = SendNode.getName()
-            beaconPosition = SendNode.getPosition()
-            # send hop message to all nodes in network
-            for ReceiveNode in NodeList:
-                if SendNode.getName() == ReceiveNode.getName():
-                    continue
-                else:
-                    # send message to all nodes in network
+    # # perform dvhop starting with beacon nodes
+    # for SendNode in NodeList:
+    #     hopCount = 0
+
+    #     # check if node is beacon
+    #     if SendNode.getBeacon():
+    #         beaconName     = SendNode.getName()
+    #         beaconPosition = SendNode.getPosition()
+    #         # send hop message to all nodes in network
+    #         for ReceiveNode in NodeList:
+    #             if SendNode.getName() == ReceiveNode.getName():
+    #                 continue
+    #             else:
+    #                 # send message to all nodes in network
                     
-                    if NodeInRange(SendNode, ReceiveNode):
-                        hopCount += 1
-                        SendNode.sendHopMessage(ReceiveNode, beaconName, beaconPosition, hopCount)
-                        SendNode = ReceiveNode
+    #                 if NodeInRange(SendNode, ReceiveNode):
+    #                     hopCount += 1
+    #                     SendNode.sendHopMessage(ReceiveNode, beaconName, beaconPosition, hopCount)
+    #                     SendNode = ReceiveNode
             
 
     # check node List
